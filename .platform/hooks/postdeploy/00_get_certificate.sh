729a24df6923b27eba5b557c0f4ea520bb633966
sethbr11 <sethabrock@gmail.com>
2023-12-04 14:16:41 -0700

    Update 00_get_certificate.sh

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d mentalhealthprovo.is404.net --nginx --agree-tos --email sethabrock@gmail.com
+sudo certbot -n -d mentalhealthprovo.is404.net --nginx --agree-tos --email sab716@byu.edu
