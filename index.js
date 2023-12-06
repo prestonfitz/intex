c0f4f330a8a151c2b3ebe74a96499999218a22fc
afank14 <103207126+afank14@users.noreply.github.com>
2023-12-06 12:55:49 -0700

    survey fix round 99

--- index.js
+++ index.js
@@ -278,7 +278,7 @@ app.post("/newSurvey", async (req, res)=> {
   // all will have the same participant_id
   await Promise.all(checkboxValuesOrgs.map(async (affiliationNum) => {
     await knex("ParticipantOrganizations").insert({
-      Participant_ID: participant_id.participant_id,
+      Participant_ID: participant_id.Participant_ID,
       Affiliation_Num: affiliationNum
     });
   }));
@@ -296,7 +296,7 @@ app.post("/newSurvey", async (req, res)=> {
 
   await Promise.all(checkboxValuesSocs.map(async (platformNum) => {
     await knex("ParticipantPlatforms").insert({
-      Participant_ID: participant_id.participant_id,
+      Participant_ID: participant_id.Participant_ID,
       Platform_Num: platformNum
     });
   }));
